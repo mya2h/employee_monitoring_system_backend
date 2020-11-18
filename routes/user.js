fa4239bbe6f76937ecbff16a2ab4981d41a68d16
kalkidan Getinet <halle2019newon22@gmail.com>
2020-11-18 08:05:21 +0000

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -5,10 +5,11 @@ const userController = require("../controller/user");
 // routes
 router.post("/authenticate", userController.login);
 router.post("/register", userController.register);
-// router.get("/", userController.getAll);
-// router.get("/current", userController.getCurrent);
-// router.get("/:id", userController.getById);
-// router.put("/:id", userController.update);
+router.get("/logout", userController.logout);
+router.get("/forgot", userController.getForgot);
+router.post("/forgot", userController.forgot);
+router.get("/reset/:token", userController.getresetwithtoken );
+router.post("/reset/:token", userController.postresetwithtoken );
 // router.delete("/:id", userController._delete);
 
 module.exports = router;
