a5a1d2ffbd07f3f1fcefb7764879834985faf60d
melkambeyene <learntechwithmelkam@gmail.com>
2020-12-04 14:40:31 +0300

    update on serverjs

--- server.js
+++ server.js
@@ -4,61 +4,40 @@ var fs = require("fs");
 const PORT = process.env.PORT || 5000;
 
 // const mongoUtil = require("./dao/dbConnection");
-// const userRouter = require("./routes/user");
+const userRouter = require("./routes/user");
 
 const indexRouter = require("./routes/index");
-
-<<<<<<< HEAD
-// const deviceRouter = require("./routes/device");
-// const categoryRouter = require("./routes/category");
-// const resourceRouter = require("./routes/resource");
-// const HRRouter = require("./routes/HRpersonnel");
+const deviceRouter = require("./routes/device");
+const categoryRouter = require("./routes/category");
+// const memberRouter = require("./routes/member");
+const resourceRouter = require("./routes/resource");
+const HRRouter = require("./routes/HRpersonnel");
 // const NTrackRouter = require("./routes/notTrack");
+
 var flash = require('express-flash');
 var passport = require('passport');
 var session = require('express-session')
-
-
-=======
-const deviceRouter = require("./routes/device");
-const categoryRouter = require("./routes/category");
-const resourceRouter = require("./routes/resource");
-const HRRouter = require("./routes/HRpersonnel");
-const NTrackRouter = require("./routes/notTrack");
 var flash = require("express-flash");
 var passport = require("passport");
 var session = require("express-session");
->>>>>>> 4f29a8822b7147d18bed904067b77ae1ded59097
+
 
 const bodyparser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 connectDB();
-// mongoUtil.connectToServer(function (err, client) {
-//   if (err) console.log(err);
-//   console.log("Database connected");
-//   // start the rest of your app here
-// });
 
-app.use((req, res, next) => {
-  // Control access to clients
-  res.header("Access-Control-Allow-Origin", "*");
-
-  // Control allowed headers
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-
-  if (req.method == "OPTIONS") {
-    // Set allowed HTTP methods
-    req.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-    return res.status(200).json({});
-  }
-
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,X-Total-Count');
+  res.header('Access-Control-Expose-Headers', 'X-Total-Count')
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
+
+app.use(cors());
 passport.serializeUser(function (user, done) {
   done(null, user.id);
 });
@@ -90,37 +69,21 @@ app.post("/", (req, res) => {
 //routes
 
 app.use("/api/v1",indexRouter);
-
-// app.use("/api/users", require("./routes/user"));
-<<<<<<< HEAD
-// app.use("/api/deviceUsers", require("./routes/deviceUsers"));
-
-
-
-app.listen(PORT, () => {
-  console.log(`The app is running on port ${PORT}`);
-});
-
-
-// app.use("/api/user", userRouter);
-// app.use("/api/resource", resourceRouter);
-// app.use("/api/device", deviceRouter);
-// app.use("/api/category", categoryRouter);
-// app.use("/api/HR", HRRouter);
-// app.use("/api/NotTrack", NTrackRouter);
-
-=======
-
 app.use("/api/user", userRouter);
 app.use("/api/resource", resourceRouter);
 app.use("/api/device", deviceRouter);
 app.use("/api/category", categoryRouter);
+
+// app.use("/api/member", memberRouter);
+app.use("/api/HR", HRRouter);
+// app.use("/api/NotTrack", NTrackRouter);
+
+
 app.use("/api/deviceUsers", require("./routes/deviceUsers"));
 app.use("/api/changedFiles", require("./routes/changedFiles"));
 app.use("/api/activeWindows", require("./routes/activeWindows"));
 app.use("/api/HR", HRRouter);
-app.use("/api/NotTrack", NTrackRouter);
+// app.use("/api/NotTrack", NTrackRouter);
 app.listen(PORT, () => {
-  console.log(`The app is running on port ${PORT}`);
+  console.log("The app is running on port ${PORT}");
 });
->>>>>>> 4f29a8822b7147d18bed904067b77ae1ded59097
